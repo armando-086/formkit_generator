cc16c89087d422fd2a9629b881047d3b8adeddd7
armando086 <aor860907@gmail.com>
2025-12-03 18:55:32 -0300

    feat: Corrección de conflicto de dependencias y configuración inicial de CodeGen

--- lib/type_checker/formkit_target_checker.dart
+++ lib/type_checker/formkit_target_checker.dart
@@ -1,7 +1,5 @@
 import 'package:source_gen/source_gen.dart';
 
-/// TypeChecker para la anotación que ahora reside en el paquete principal.
 const formKitTargetChecker = TypeChecker.fromUrl(
   'package:formkit/src/annotation/formkit_target.dart#FormKitTarget',
 );
-
